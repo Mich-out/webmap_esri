b0d252d7401df24254232db9445a22c59da36ee6
Mich-out <125644348+Mich-out@users.noreply.github.com>
2023-11-28 12:03:21 +0100

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,5 +1,6 @@
 <?php
-  header('Status: 301 Moved Permanently', false, 301);      
-  header('Location: https://aoprestlse.maps.arcgis.com/apps/webappviewer/index.html?id=687a243e97db496fb917962b77045128');      
-  exit();
+
+  header('HTTP/1.1 301 Moved Permanently');      
+  header('Location: https://aoprestlse.maps.arcgis.com/apps/webappviewer/index.html?id=687a243e97db496fb917962b77045128');
+
 ?>
